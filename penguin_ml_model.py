9b2ba7585442cf75aefbe8ffc36d765ccd5a2cda
tutyasmawaty <93817507+tutyasmawaty@users.noreply.github.com>
2021-11-07 05:09:36 +0100

    Add files via upload

---
+++ penguin_ml_model.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+penguin_df = pd.read_csv("penguins.csv")
+#print(penguin_df.head())
+#print(penguin_df.tail())
+
+#remove nan value
+penguin_df.dropna(inplace=True)
+
+output = penguin_df['species'] #target
+features = penguin_df[['island','bill_length_mm','flipper_length_mm','body_mass_g','sex']]
+
+#original after cleaning nan value
+#print(output.tail())
+#print(features.head())
+
+#feature after
+features = pd.get_dummies(features)
+#print(features.tail())
+
+output, uniques = pd.factorize(output)
+#print(uniques)
+#print(output)
+
+#train test split
+x_train, x_test, y_train, y_test = train_test_split(features, output, test_size=0.2)
+
+rfc = RandomForestClassifier(random_state=15)
+rfc.fit(x_train, y_train)
+
+#predict parameter using test set data
+y_pred = rfc.predict(x_test)
+score = accuracy_score(y_pred, y_test)
+print ("Our accuracy score for this model is {}".format(score))
+
+#save the penguin RF RandomForestClassifierr
+rf_pickle=open("random_forest_penguin.pickle",'wb')
+pickle.dump(rfc, rf_pickle)
+rf_pickle.close()
+
+output_pickle = open ('output_penguin.pickle','wb')
+pickle.dump(uniques, output_pickle)
+output_pickle.close()
+
+print('Success')
---
+++ penguin_ml_streamlit.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pickle
+st.title('Penguin Classifier')
+st.write("This app uses 6 inputs to predict the species of penguin using a "
+"model built on the Palmer's Penguin's dataset. Use the form below to get started!")
+
+# load pickle files
+rf_pickle = open('random_forest_penguin.pickle', 'rb')
+map_pickle = open('output_penguin.pickle', 'rb')
+rfc = pickle.load(rf_pickle)
+unique_penguin_mapping = pickle.load(map_pickle)
+rf_pickle.close()
+map_pickle.close()
+
+# user input
+island = st.selectbox('Penguin Island', options=['Biscoe', 'Dream', 'Torgerson'])
+sex = st.selectbox('Sex', options=['Female', 'Male'])
+bill_length = st.number_input('Bill Length (mm)', min_value=0)
+bill_depth = st.number_input('Bill Depth (mm)', min_value=0)
+flipper_length = st.number_input('Flipper Length (mm)', min_value=0)
+body_mass = st.number_input('Body Mass (g)', min_value=0)
+
+island_biscoe, island_dream, island_torgerson = 0, 0, 0
+# mapping the island data
+if island == 'Biscoe':
+    island_biscoe = 1
+elif island == 'Dream':
+    island_dream = 1
+elif island == 'Torgerson':
+    island_torgerson = 1
+
+# mapping the sex data
+sex_female, sex_male = 0, 0
+if sex == 'Female':
+    sex_female = 1
+elif sex == 'Male':
+    sex_male = 1
+
+if st.button("Classify!"):
+    new_prediction = rfc.predict([[bill_length, bill_depth, flipper_length,body_mass,
+    island_biscoe, island_dream,island_torgerson, sex_female, sex_male]])
+    prediction_species = unique_penguin_mapping[new_prediction][0]
+    st.write('We predict your penguin is of the {} species'.format(prediction_species))
